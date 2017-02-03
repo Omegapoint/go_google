19f6c89033750acfc4a5e834ad523ff9a4cc98d2
Max <maxsch@omegapoint.se>
2017-02-03 16:27:13 +0100

    Trying to get datastores working aswell

--- main.go
+++ main.go
@@ -7,10 +7,14 @@ import (
 	"net/http"
 	"os"
 
+	_ "golang.org/x/net/context"   //for datastore
+	_ "golang.org/x/oauth2/google" //for Datastore
+
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/log"
 
 	_ "github.com/go-sql-driver/mysql" //Needed for SQL connection
+	_ "google.golang.org/appengine/remote_api"
 )
 
 func init() {
@@ -61,42 +65,6 @@ func what(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "sup")
 }
 
-/*func main() {
-
-	connectionName := mustGetenv("CLOUDSQL_CONNECTION_NAME")
-	user := mustGetenv("CLOUDSQL_USER")
-	password := os.Getenv("CLOUDSQL_PASSWORD")
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@cloudsql(%s)/", user, password, connectionName))
-	if err != nil {
-		fmt.Println("Something connect open")
-	}
-	err = db.Ping()
-	if err != nil {
-		fmt.Println("stuff didnt work")
-	}
-	fmt.Println("stuff")
-	var name string
-	var quantity int
-	rows, err := db.Query("SELECT name, quantity FROM godata")
-	if err != nil {
-		fmt.Print("fail in select")
-	}
-
-	defer rows.Close()
-
-	for rows.Next() {
-		err := rows.Scan(&name, &quantity)
-		if err != nil {
-			fmt.Println("fel i scan")
-		}
-		fmt.Println(name, quantity)
-	}
-
-	//http.HandleFunc("/", hello)
-	//http.HandleFunc("/sup", what)
-	//http.ListenAndServe(":8000", nil)
-}
-*/
 func mustGetenv(k string) string {
 	v := os.Getenv(k)
 	if v == "" {
